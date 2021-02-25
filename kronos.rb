a4622557429877a2c13a1a36c28364b76f0b6395
Tacio Costa <taciomcosta@gmail.com>
2021-02-25 19:46:46 -0300

    fix: parsing plist error

--- kronos.rb
+++ kronos.rb
@@ -2,7 +2,7 @@ class Kronos < Formula
   desc "Cross-platform job scheduler for cronjobs"
   homepage "https://github.com/taciomcosta/kronos"
   url "https://github.com/taciomcosta/kronos/releases/download/0.1.0/kronos-0.1.0-darwin_amd64.tar.gz"
-  sha256 "3fd0e9a19a00aef2390d132605b154b5af98dd99a80523adffcf30e15c5c4742"
+  sha256 "4df4dbfa6df4aa963ee2a81fa2f73b205beba3d4dbcf59de367db65b8b537e05"
   license "MIT"
   bottle :unneeded
 
@@ -11,20 +11,24 @@ class Kronos < Formula
     bin.install "kronos"
     system "mkdir -p /etc/kronos"
     system "mkdir -p /var/lib/kronos"
+    (var/"kronos").mkpath
+    (etc/"kronos").mkpath
+    print var
   end
 
   plist_options manual: "kronosd"
 
   def plist
     <<~EOS
-      <?xml version='1.0' encoding='UTF-8'?>
-      <!DOCTYPE plist PUBLIC \"-//Apple Computer//DTD PLIST 1.0//EN\" \"http://www.apple.com/DTDs/PropertyList-1.0.dtd\" >
-      <plist version='1.0'>
-        <dict>
-          <key>Label</key><string>#{plist_name}</string>
-          <key>Program</key><string>#{opt_bin}/kronosd</string>
-          <key>RunAtLoad</key><true/>
-        </dict>
+      <?xml version="1.0" encoding="UTF-8"?>
+      <!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
+      <plist version="1.0">
+      <dict>
+        <key>KeepAlive</key> <true/>
+        <key>Label</key> <string>#{plist_name}</string>
+        <key>Program</key> <string>/usr/local/bin/kronosd</string>
+        <key>RunAtLoad</key> <true/>
+      </dict>
       </plist>
     EOS
   end
